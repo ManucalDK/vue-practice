c5eae6e3601badd1e451a6fa3a57727482fe8bff
Manuel C <macalambas@gmail.com>
2020-01-24 17:24:56 -0500

    modelos y evento click

--- capitulo1/app.js
+++ capitulo1/app.js
@@ -6,6 +6,18 @@ const app = new Vue({
             {nombre:'Pera', cantidad:23},
             {nombre:'Manzana', cantidad:4},
             {nombre:'Platano', cantidad:0}
-        ]
+        ],
+        nuevaFruta: ''
+    },
+    methods:{
+        agregarFruta (){
+            console.log("agregaste una fruta");
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad : Math.floor(Math.random() * 10)
+            })
+        },
+        otroMetodo(){
+
+        }
     }
 })
